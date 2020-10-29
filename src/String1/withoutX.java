e6e03c048cca3c1a6985d343d90e98893cbf4dc2
SabiaE <sabia.egan@gmail.com>
2020-10-29 10:53:00 +0000

    last 2 String problems

--- src/String1/withoutX.java
+++ src/String1/withoutX.java
@@ -1,4 +1,33 @@
 package String1;
 
 public class withoutX {
+    public String withoutX(String str) {
+        if(str.length() == 0){
+            return str;
+        }
+
+        if(str.length() >1) {
+            String front = str.substring(0,1);
+            String end = str.substring(str.length()-1);
+
+            if(front.equals("x") && end.equals("x")) {
+                return str.substring(1,str.length()-1);
+            }
+
+            if(front.equals("x")) {
+                return str.substring(1);
+            }
+
+            if(end.equals("x")) {
+                return str.substring(0, str.length()-1);
+            }
+
+            else {
+                return str;
+            }
+
+        }
+        return "";
+    }
+
 }
--- src/String1/withoutX2.java
+++ src/String1/withoutX2.java
@@ -1,4 +1,17 @@
 package String1;
 
 public class withoutX2 {
+    public String withoutX2(String str) {
+        if(str.length()>1 && str.substring(0,2).equals("xx")) {
+            return str.substring(2);
+        } else if (str.length()>1 && str.substring(1,2).equals("x")) {
+            return str.substring(0,1) + str.substring(2);
+        }
+        else if(str.length()>0 && str.substring(0,1).equals("x")) {
+            return str.substring(1);
+        } else {
+            return str;
+        }
+    }
+
 }
